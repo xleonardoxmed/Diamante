a2c6d9558fbc9e71e69d76e955a7fca698fdd61a
leonardo medeiros <leonarodmed@hotmail.com>
2025-03-25 17:06:45 -0300

    Esboço do projeto, com painel, solicitação, validação e loop estruturado.

--- Diamante.ConsoleApp/Program.cs
+++ Diamante.ConsoleApp/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("==========================================================");
+                Console.WriteLine("A pressão determina se um carvão se tornará diamente");
+                Console.WriteLine("==========================================================");
+                bool success;
+                int centerNumber = 0;
+
+                do
+                {
+                    Console.WriteLine("Insira o número ÍMPAR de letras 'x' que irá compor o centro da jóia: ");
+                    string entry = Console.ReadLine()!;
+                    success = int.TryParse(entry, out centerNumber);
+
+                    if (!success || centerNumber % 2 == 0 || centerNumber <= 0)
+                    {
+                        Console.WriteLine("------------------------------------------------------");
+                        Console.WriteLine("Erro! Insira somente números ÍMPARES e INTEIROS.");
+                        Console.WriteLine("------------------------------------------------------");
+                        continue;
+                    }
+                           
+                } while(!success || centerNumber % 2 == 0 || centerNumber <= 0);
+
+                Console.WriteLine("Deseja criar um novo diamante? (S/N)");
+                string toContinue = Console.ReadLine()!.ToUpper();
+                if (toContinue != "S")
+                    break;
+            }
         }
     }
 }
