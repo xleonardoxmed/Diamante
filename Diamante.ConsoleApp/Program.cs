395ddce0cea451e625d6e1e72cc5af532f19bc27
leonardo medeiros <leonarodmed@hotmail.com>
2025-03-25 16:25:29 -0300

    Add project files.

---
+++ Diamante.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Diamante.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
